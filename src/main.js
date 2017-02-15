3e9c044b9460e988bd0bd20c2bfa9d30fe6905c0
lihuaqi <66619470@qq.com>
2017-02-15 13:16:56 +0800

    '用豆瓣公开的api展示数据'

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 import App from './App.vue'
-
+import VueResource from 'vue-resource'
+Vue.use(VueResource);
 Vue.use(ElementUI)
 
 new Vue({
